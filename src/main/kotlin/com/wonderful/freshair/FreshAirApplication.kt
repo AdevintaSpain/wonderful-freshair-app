ivan-lorenz/ada-freshair

Air quality comparer implemented

src/main/kotlin/com/ada/freshair/FreshAirApplication.kt
@@ -5,6 +5,7 @@ import com.ada.freshair.domain.CityAirQualityService
 import com.ada.freshair.domain.CityGeoCodingService
 import com.ada.freshair.infrastructure.api.OWMAirQualityForecastService
 import com.ada.freshair.infrastructure.api.OWMCityGeoCodingService
+import com.ada.freshair.infrastructure.console.AirQualityComparer
 import com.ada.freshair.infrastructure.console.AirQualityComputation
 import java.net.URL
 
@@ -17,6 +18,7 @@ fun main(args: Array<String>) {
     val airQualityForecastService: AirQualityForecastService = OWMAirQualityForecastService(baseUrl, apiKey)
     val cityAirQualityService = CityAirQualityService(cityGeoCodingService, airQualityForecastService)
     val airQualityComputation = AirQualityComputation(cityAirQualityService)
+    val airQualityComparer = AirQualityComparer(cityAirQualityService)
 
     val arguments = args.fold(Pair(emptyMap<String, List<String>>(), "")) { (map, lastKey), elem ->
         if (elem.startsWith("-"))  Pair(map + (elem to emptyList()), elem)
@@ -27,6 +29,7 @@ fun main(args: Array<String>) {
         val cities = arguments[argument] ?: emptyList()
         when (argument) {
             "--city" -> airQualityComputation.compute(cities)
+            "--compare" -> airQualityComparer.compare(cities)
         }
     }
 }

src/main/kotlin/com/ada/freshair/infrastructure/console/AirQualityComparer.kt
@@ -1,0 +1,21 @@
+package com.ada.freshair.infrastructure.console
+
+import arrow.core.filterOption
+import com.ada.freshair.domain.AirQualityIndex
+import com.ada.freshair.domain.CityAirQualityService
+import com.ada.freshair.infrastructure.City
+
+class AirQualityComparer(
+    private val cityAirQualityService: CityAirQualityService) {
+
+    fun compare(cities: List<String>) {
+        val airQualityIndex = cities
+            .map { City.fromParameter(it) }
+            .map { cityAirQualityService.averageIndex(it) }
+            .filterOption()
+            .maxWithOrNull(Comparator.comparing(AirQualityIndex::index))
+
+        if (airQualityIndex != null)
+            println("${airQualityIndex.cityName} has the cleaner air quality index.")
+    }
+}

src/test/kotlin/com/ada/freshair/infrastructure/console/AirQualityComparerTest.kt
@@ -1,0 +1,55 @@
+package com.ada.freshair.infrastructure.console
+
+import arrow.core.Some
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.ada.freshair.domain.AirQualityIndex
+import com.ada.freshair.domain.CityAirQualityService
+import com.ada.freshair.infrastructure.City
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+class AirQualityComparerTest {
+
+    private lateinit var cityAirQualityService: CityAirQualityService
+    private lateinit var airQualityComparer: AirQualityComparer
+
+    private val standardOut = System.out
+    private val outputStreamCaptor: ByteArrayOutputStream = ByteArrayOutputStream()
+
+    @BeforeEach
+    fun setUp() {
+        System.setOut(PrintStream(outputStreamCaptor))
+        cityAirQualityService = mock()
+        airQualityComparer = AirQualityComparer(cityAirQualityService)
+    }
+
+    @AfterEach
+    fun teardown() {
+        System.setOut(standardOut)
+    }
+
+    @Test
+    fun `should compare air quality index and get best city`() {
+        val country = "ES"
+        val barcelona = "Barcelona"
+        val barcelonaIndex = 1.50
+        val madrid = "Madrid"
+        val madridIndex = 1.49
+        val cities = listOf("$barcelona,$country", "$madrid,$country")
+        whenever(cityAirQualityService.averageIndex(City(barcelona, country)))
+            .thenReturn(Some(AirQualityIndex(barcelona, barcelonaIndex)))
+        whenever(cityAirQualityService.averageIndex(City(madrid, country)))
+            .thenReturn(Some(AirQualityIndex(madrid, madridIndex)))
+
+        airQualityComparer.compare(cities)
+
+        assertThat(outputStreamCaptor.toString().trim())
+            .isEqualTo("$barcelona has the cleaner air quality index.")
+    }
+}

AdevintaSpain/wonderful-freshair-app

src/main/kotlin/com/wonderful/freshair/FreshAirApplication.kt
@@ -5,6 +5,7 @@ import com.wonderful.freshair.domain.CityAirQualityService
 import com.wonderful.freshair.domain.CityGeoCodingService
 import com.wonderful.freshair.infrastructure.api.OWMAirQualityForecastService
 import com.wonderful.freshair.infrastructure.api.OWMCityGeoCodingService
+import com.wonderful.freshair.infrastructure.console.AirQualityComparer
 import com.wonderful.freshair.infrastructure.console.AirQualityComputation
 import java.net.URL
 
@@ -17,6 +18,7 @@ fun main(args: Array<String>) {
   val airQualityForecastService: AirQualityForecastService = OWMAirQualityForecastService(baseUrl, apiKey)
   val cityAirQualityService = CityAirQualityService(cityGeoCodingService, airQualityForecastService)
   val airQualityComputation = AirQualityComputation(cityAirQualityService)
+  val airQualityComparer = AirQualityComparer(cityAirQualityService)
 
   val arguments = args.fold(Pair(emptyMap<String, List<String>>(), "")) { (map, lastKey), elem ->
     if (elem.startsWith("-")) Pair(map + (elem to emptyList()), elem)
@@ -27,6 +29,7 @@ fun main(args: Array<String>) {
     val cities = arguments[argument] ?: emptyList()
     when (argument) {
       "--city" -> airQualityComputation.compute(cities)
+      "--compare" -> airQualityComparer.compare(cities)
     }
   }
 }

src/main/kotlin/com/wonderful/freshair/infrastructure/console/AirQualityComparer.kt
@@ -1,0 +1,22 @@
+package com.wonderful.freshair.infrastructure.console
+
+import arrow.core.filterOption
+import com.wonderful.freshair.domain.AirQualityIndex
+import com.wonderful.freshair.domain.CityAirQualityService
+import com.wonderful.freshair.infrastructure.City
+
+class AirQualityComparer(
+    private val cityAirQualityService: CityAirQualityService
+) {
+
+    fun compare(cities: List<String>) {
+        val airQualityIndex = cities
+            .map { City.fromParameter(it) }
+            .map { cityAirQualityService.averageIndex(it) }
+            .filterOption()
+            .maxWithOrNull(Comparator.comparing(AirQualityIndex::index))
+
+        if (airQualityIndex != null)
+            println("${airQualityIndex.cityName} has the cleaner air quality index.")
+    }
+}

src/test/kotlin/com/wonderful/freshair/infrastructure/console/AirQualityComparerTest.kt
@@ -1,0 +1,55 @@
+package com.wonderful.freshair.infrastructure.console
+
+import arrow.core.Some
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.wonderful.freshair.domain.AirQualityIndex
+import com.wonderful.freshair.domain.CityAirQualityService
+import com.wonderful.freshair.infrastructure.City
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+class AirQualityComparerTest {
+
+    private lateinit var cityAirQualityService: CityAirQualityService
+    private lateinit var airQualityComparer: AirQualityComparer
+
+    private val standardOut = System.out
+    private val outputStreamCaptor: ByteArrayOutputStream = ByteArrayOutputStream()
+
+    @BeforeEach
+    fun setUp() {
+        System.setOut(PrintStream(outputStreamCaptor))
+        cityAirQualityService = mock()
+        airQualityComparer = AirQualityComparer(cityAirQualityService)
+    }
+
+    @AfterEach
+    fun teardown() {
+        System.setOut(standardOut)
+    }
+
+    @Test
+    fun `should compare air quality index and get best city`() {
+        val country = "ES"
+        val barcelona = "Barcelona"
+        val barcelonaIndex = 1.50
+        val madrid = "Madrid"
+        val madridIndex = 1.49
+        val cities = listOf("$barcelona,$country", "$madrid,$country")
+        whenever(cityAirQualityService.averageIndex(City(barcelona, country)))
+            .thenReturn(Some(AirQualityIndex(barcelona, barcelonaIndex)))
+        whenever(cityAirQualityService.averageIndex(City(madrid, country)))
+            .thenReturn(Some(AirQualityIndex(madrid, madridIndex)))
+
+        airQualityComparer.compare(cities)
+
+        assertThat(outputStreamCaptor.toString().trim())
+            .isEqualTo("$barcelona has the cleaner air quality index.")
+    }
+}