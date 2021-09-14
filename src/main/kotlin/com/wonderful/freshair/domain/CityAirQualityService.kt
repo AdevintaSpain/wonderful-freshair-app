ivan-lorenz/ada-freshair

Air quality comparer fixed using Either

src/main/kotlin/com/ada/freshair/domain/CityAirQualityService.kt
@@ -1,6 +1,7 @@
 package com.ada.freshair.domain
 
-import arrow.core.Option
+import arrow.core.Either
+import com.ada.freshair.domain.error.ApplicationError
 import com.ada.freshair.infrastructure.City
 import java.math.BigDecimal
 import java.math.RoundingMode
@@ -14,12 +15,12 @@ class CityAirQualityService(
     private val cityGeocodingService: CityGeoCodingService,
     private val airQualityForecastService: AirQualityForecastService
 ) {
-    fun averageIndex(city: City): Option<AirQualityIndex> =
+    fun averageIndex(city: City): Either<ApplicationError, AirQualityIndex> =
         cityGeocodingService.getGeoCoordinates(city)
             .flatMap { airQualityForecastService.getAirQualityForecast(it.coordinates) }
             .map {
                 AirQualityIndex(city.name, it
                     .map { forecast -> forecast.index }
                     .average())
-            }
+            }.toEither { ApplicationError() }
 }

src/main/kotlin/com/ada/freshair/domain/error/ApplicationError.kt
@@ -1,0 +1,3 @@
+package com.ada.freshair.domain.error
+
+class ApplicationError

src/test/kotlin/com/ada/freshair/domain/CityAirQualityServiceTest.kt
@@ -2,8 +2,12 @@ package com.ada.freshair.domain
 
 import arrow.core.None
 import arrow.core.Some
+import arrow.core.left
+import arrow.core.right
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
+import com.ada.freshair.domain.error.ApplicationError
 import com.ada.freshair.infrastructure.City
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -39,7 +43,7 @@ class CityAirQualityServiceTest {
 
         val airQualityIndex = cityAirQualityService.averageIndex(city)
 
-        assertThat(airQualityIndex).isEqualTo(Some(AirQualityIndex(cityName, 1.5)))
+        assertThat(airQualityIndex).isEqualTo(AirQualityIndex(cityName, 1.5).right())
     }
 
     @Test
@@ -49,7 +53,7 @@ class CityAirQualityServiceTest {
         val city = City(cityName, countryCode)
         whenever(cityGeocodingService.getGeoCoordinates(city)).thenReturn(None)
 
-        assertThat(cityAirQualityService.averageIndex(city)).isEqualTo(None)
+        assertThat(cityAirQualityService.averageIndex(city)).isInstanceOf(ApplicationError().left()::class)
     }
 
     @Test
@@ -62,6 +66,6 @@ class CityAirQualityServiceTest {
         whenever(cityGeocodingService.getGeoCoordinates(city)).thenReturn(Some(cityGeocoded))
         whenever(airQualityForecastService.getAirQualityForecast(coordinates)).thenReturn(None)
 
-        assertThat(cityAirQualityService.averageIndex(city)).isEqualTo(None)
+        assertThat(cityAirQualityService.averageIndex(city)).isInstanceOf(ApplicationError().left()::class.java)
     }
 }

src/test/kotlin/com/ada/freshair/infrastructure/console/AirQualityComparerTest.kt
@@ -1,10 +1,12 @@
 package com.ada.freshair.infrastructure.console
 
-import arrow.core.Some
+import arrow.core.left
+import arrow.core.right
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.ada.freshair.domain.AirQualityIndex
 import com.ada.freshair.domain.CityAirQualityService
+import com.ada.freshair.domain.error.ApplicationError
 import com.ada.freshair.infrastructure.City
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -43,13 +45,31 @@ class AirQualityComparerTest {
         val madridIndex = 1.49
         val cities = listOf("$barcelona,$country", "$madrid,$country")
         whenever(cityAirQualityService.averageIndex(City(barcelona, country)))
-            .thenReturn(Some(AirQualityIndex(barcelona, barcelonaIndex)))
+            .thenReturn(AirQualityIndex(barcelona, barcelonaIndex).right())
         whenever(cityAirQualityService.averageIndex(City(madrid, country)))
-            .thenReturn(Some(AirQualityIndex(madrid, madridIndex)))
+            .thenReturn(AirQualityIndex(madrid, madridIndex).right())
 
         airQualityComparer.compare(cities)
 
         assertThat(outputStreamCaptor.toString().trim())
             .isEqualTo("$barcelona has the cleaner air quality index.")
+    }
+
+    @Test
+    fun `should fail if cannot compute air quality for a city`() {
+        val country = "ES"
+        val barcelona = "Barcelona"
+        val barcelonaIndex = 1.50
+        val madrid = "Madrid"
+        val cities = listOf("$barcelona,$country", "$madrid,$country")
+        whenever(cityAirQualityService.averageIndex(City(barcelona, country)))
+            .thenReturn(AirQualityIndex(barcelona, barcelonaIndex).right())
+        whenever(cityAirQualityService.averageIndex(City(madrid, country)))
+            .thenReturn(ApplicationError().left())
+
+        airQualityComparer.compare(cities)
+
+        assertThat(outputStreamCaptor.toString().trim())
+            .isEqualTo("Cannot compare air quality due to application error.")
     }
 }

src/test/kotlin/com/ada/freshair/infrastructure/console/AirQualityComputationTest.kt
@@ -1,12 +1,13 @@
 package com.ada.freshair.infrastructure.console
 
-import arrow.core.None
-import arrow.core.Some
+import arrow.core.left
+import arrow.core.right
 import assertk.assertThat
 import assertk.assertions.isEmpty
 import assertk.assertions.isEqualTo
 import com.ada.freshair.domain.AirQualityIndex
 import com.ada.freshair.domain.CityAirQualityService
+import com.ada.freshair.domain.error.ApplicationError
 import com.ada.freshair.infrastructure.City
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -47,7 +48,7 @@ class AirQualityComputationTest {
         val index = 1.50
         val expectedIndex = BigDecimal(index).setScale(2, RoundingMode.HALF_UP)
         whenever(cityAirQualityService.averageIndex(City(city, country)))
-            .thenReturn(Some(AirQualityIndex(city, index)))
+            .thenReturn(AirQualityIndex(city, index).right())
 
         airQualityComputation.compute(cities)
 
@@ -60,7 +61,7 @@ class AirQualityComputationTest {
         val city = "Barcelona"
         val country = "ES"
         val cities = listOf("$city,$country")
-        whenever(cityAirQualityService.averageIndex(City(city, country))).thenReturn(None)
+        whenever(cityAirQualityService.averageIndex(City(city, country))).thenReturn(ApplicationError().left())
 
         airQualityComputation.compute(cities)
 

AdevintaSpain/wonderful-freshair-app

src/main/kotlin/com/wonderful/freshair/domain/CityAirQualityService.kt
@@ -1,9 +1,8 @@
 package com.wonderful.freshair.domain
 
-import arrow.core.None
-import arrow.core.Option
-import arrow.core.Some
+import arrow.core.Either
 import arrow.core.computations.option
+import com.wonderful.freshair.domain.error.ApplicationError
 import com.wonderful.freshair.infrastructure.City
 import java.math.BigDecimal
 import java.math.RoundingMode
@@ -17,13 +16,13 @@ class CityAirQualityService(
     private val cityGeocodingService: CityGeoCodingService,
     private val airQualityForecastService: AirQualityForecastService
 ) {
-    fun averageIndex(city: City): Option<AirQualityIndex> = option.eager {
+    fun averageIndex(city: City): Either<ApplicationError, AirQualityIndex> = option.eager<AirQualityIndex> {
         val (_, _, coordinates) = cityGeocodingService.getGeoCoordinates(city).bind()
         val forecasts = airQualityForecastService.getAirQualityForecast(coordinates).bind()
         AirQualityIndex(
             city.name,
             forecasts.map { forecast -> forecast.index }.average()
         )
-    }
+    }.toEither { ApplicationError() }
 
 }

src/main/kotlin/com/wonderful/freshair/domain/error/ApplicationError.kt
@@ -1,0 +1,3 @@
+package com.wonderful.freshair.domain.error
+
+class ApplicationError

src/test/kotlin/com/wonderful/freshair/domain/CityAirQualityServiceTest.kt
@@ -2,16 +2,17 @@ package com.wonderful.freshair.domain
 
 import arrow.core.None
 import arrow.core.Some
+import arrow.core.left
+import arrow.core.right
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import assertk.assertions.isNull
 import com.wonderful.freshair.infrastructure.City
+import assertk.assertions.isInstanceOf
+import com.wonderful.freshair.domain.error.ApplicationError
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
-import java.math.BigDecimal
-import java.math.RoundingMode
 
 class CityAirQualityServiceTest {
 
@@ -42,21 +43,21 @@ class CityAirQualityServiceTest {
 
         val airQualityIndex = cityAirQualityService.averageIndex(city)
 
-        assertThat(airQualityIndex).isEqualTo(Some(AirQualityIndex(cityName, 1.5)))
+        assertThat(airQualityIndex).isEqualTo(AirQualityIndex(cityName, 1.5).right())
     }
 
     @Test
-    fun `should return none if city does not exist`() {
+    fun `should return left if city does not exist`() {
         val cityName = "Barcelona"
         val countryCode = "ES"
         val city = City(cityName, countryCode)
         whenever(cityGeocodingService.getGeoCoordinates(city)).thenReturn(None)
 
-        assertThat(cityAirQualityService.averageIndex(city)).isEqualTo(None)
+        assertThat(cityAirQualityService.averageIndex(city)).isInstanceOf(ApplicationError().left()::class)
     }
 
     @Test
-    fun `should return none if pollution data is empty`() {
+    fun `should return left if pollution data is empty`() {
         val cityName = "Barcelona"
         val countryCode = "ES"
         val city = City(cityName, countryCode)
@@ -65,6 +66,6 @@ class CityAirQualityServiceTest {
         whenever(cityGeocodingService.getGeoCoordinates(city)).thenReturn(Some(cityGeocoded))
         whenever(airQualityForecastService.getAirQualityForecast(coordinates)).thenReturn(None)
 
-        assertThat(cityAirQualityService.averageIndex(city)).isEqualTo(None)
+        assertThat(cityAirQualityService.averageIndex(city)).isInstanceOf(ApplicationError().left()::class.java)
     }
 }

src/test/kotlin/com/wonderful/freshair/infrastructure/console/AirQualityComparerTest.kt
@@ -1,10 +1,12 @@
 package com.wonderful.freshair.infrastructure.console
 
-import arrow.core.Some
+import arrow.core.left
+import arrow.core.right
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.wonderful.freshair.domain.AirQualityIndex
 import com.wonderful.freshair.domain.CityAirQualityService
+import com.wonderful.freshair.domain.error.ApplicationError
 import com.wonderful.freshair.infrastructure.City
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -43,13 +45,31 @@ class AirQualityComparerTest {
         val madridIndex = 1.49
         val cities = listOf("$barcelona,$country", "$madrid,$country")
         whenever(cityAirQualityService.averageIndex(City(barcelona, country)))
-            .thenReturn(Some(AirQualityIndex(barcelona, barcelonaIndex)))
+            .thenReturn(AirQualityIndex(barcelona, barcelonaIndex).right())
         whenever(cityAirQualityService.averageIndex(City(madrid, country)))
-            .thenReturn(Some(AirQualityIndex(madrid, madridIndex)))
+            .thenReturn(AirQualityIndex(madrid, madridIndex).right())
 
         airQualityComparer.compare(cities)
 
         assertThat(outputStreamCaptor.toString().trim())
             .isEqualTo("$barcelona has the cleaner air quality index.")
+    }
+
+    @Test
+    fun `should fail if cannot compute air quality for a city`() {
+        val country = "ES"
+        val barcelona = "Barcelona"
+        val barcelonaIndex = 1.50
+        val madrid = "Madrid"
+        val cities = listOf("$barcelona,$country", "$madrid,$country")
+        whenever(cityAirQualityService.averageIndex(City(barcelona, country)))
+            .thenReturn(AirQualityIndex(barcelona, barcelonaIndex).right())
+        whenever(cityAirQualityService.averageIndex(City(madrid, country)))
+            .thenReturn(ApplicationError().left())
+
+        airQualityComparer.compare(cities)
+
+        assertThat(outputStreamCaptor.toString().trim())
+            .isEqualTo("Cannot compare air quality due to application error.")
     }
 }

src/test/kotlin/com/wonderful/freshair/infrastructure/console/AirQualityComputationTest.kt
@@ -1,12 +1,13 @@
 package com.wonderful.freshair.infrastructure.console
 
-import arrow.core.None
-import arrow.core.Some
+import arrow.core.left
+import arrow.core.right
 import assertk.assertThat
 import assertk.assertions.isEmpty
 import assertk.assertions.isEqualTo
 import com.wonderful.freshair.domain.AirQualityIndex
 import com.wonderful.freshair.domain.CityAirQualityService
+import com.wonderful.freshair.domain.error.ApplicationError
 import com.wonderful.freshair.infrastructure.City
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -46,7 +47,8 @@ class AirQualityComputationTest {
         val cities = listOf("$city,$country")
         val index = 1.50
         val expectedIndex = BigDecimal(index).setScale(2, RoundingMode.HALF_UP)
-        whenever(cityAirQualityService.averageIndex(City(city, country))).thenReturn(Some(AirQualityIndex(city, index)))
+        whenever(cityAirQualityService.averageIndex(City(city, country)))
+            .thenReturn(AirQualityIndex(city, index).right())
 
         airQualityComputation.compute(cities)
 
@@ -59,7 +61,7 @@ class AirQualityComputationTest {
         val city = "Barcelon"
         val country = "ES"
         val cities = listOf("$city,$country")
-        whenever(cityAirQualityService.averageIndex(City(city, country))).thenReturn(None)
+        whenever(cityAirQualityService.averageIndex(City(city, country))).thenReturn(ApplicationError().left())
 
         airQualityComputation.compute(cities)
 