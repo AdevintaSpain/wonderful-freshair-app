ivan-lorenz/ada-freshair

City air quality service uses now Either

src/main/kotlin/com/ada/freshair/domain/AirQualityForecastService.kt
@@ -1,9 +1,10 @@
 package com.ada.freshair.domain
 
-import arrow.core.Option
+import arrow.core.Either
+import com.ada.freshair.domain.error.ApplicationError
 
 data class AirQualityForecast(val index: Int)
 
 interface AirQualityForecastService {
-    fun getAirQualityForecast(coordinates: GeoCoordinates): Option<List<AirQualityForecast>>
+    fun getAirQualityForecast(coordinates: GeoCoordinates): Either<ApplicationError, List<AirQualityForecast>>
 }

src/main/kotlin/com/ada/freshair/infrastructure/api/OWMAirQualityForecastService.kt
@@ -1,11 +1,12 @@
 package com.ada.freshair.infrastructure.api
 
-import arrow.core.None
-import arrow.core.Option
-import arrow.core.Some
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import com.ada.freshair.domain.AirQualityForecast
 import com.ada.freshair.domain.AirQualityForecastService
 import com.ada.freshair.domain.GeoCoordinates
+import com.ada.freshair.domain.error.ApplicationError
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
@@ -29,7 +30,7 @@ class OWMAirQualityForecastService(
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     }
 
-    override fun getAirQualityForecast(coordinates: GeoCoordinates): Option<List<AirQualityForecast>> {
+    override fun getAirQualityForecast(coordinates: GeoCoordinates): Either<ApplicationError, List<AirQualityForecast>> {
         val request = HttpRequest.newBuilder()
             .uri(URL(
                 baseUrl,
@@ -45,7 +46,7 @@ class OWMAirQualityForecastService(
             .list
             .map { AirQualityForecast(it.main.aqi) }
 
-        return if (forecasts.isEmpty()) None else Some(forecasts)
+        return if (forecasts.isEmpty()) ApplicationError().left() else forecasts.right()
     }
 
 }

src/test/kotlin/com/ada/freshair/domain/AirQualityForecastServiceTest.kt
@@ -1,9 +1,11 @@
 package com.ada.freshair.domain
 
-import arrow.core.None
-import arrow.core.Some
+import arrow.core.left
+import arrow.core.right
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
+import com.ada.freshair.domain.error.ApplicationError
 import com.ada.freshair.infrastructure.api.OWMAirQualityForecastService
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
@@ -59,7 +61,7 @@ class AirQualityForecastServiceTest {
 
         val forecasts = airQualityForecastService.getAirQualityForecast(GeoCoordinates(lat, lon))
 
-        assertThat(forecasts).isEqualTo(Some(expectedAirQualityForecasts))
+        assertThat(forecasts).isEqualTo(expectedAirQualityForecasts.right())
     }
 
     @Test
@@ -78,6 +80,6 @@ class AirQualityForecastServiceTest {
 
         val airQualityForecasts = airQualityForecastService.getAirQualityForecast(GeoCoordinates(lat, lon))
 
-        assertThat(airQualityForecasts).isEqualTo(None)
+        assertThat(airQualityForecasts).isInstanceOf(ApplicationError().left()::class)
     }
 }

AdevintaSpain/wonderful-freshair-app

src/main/kotlin/com/wonderful/freshair/domain/AirQualityForecastService.kt
@@ -1,9 +1,10 @@
 package com.wonderful.freshair.domain
 
-import arrow.core.Option
+import arrow.core.Either
+import com.wonderful.freshair.domain.error.ApplicationError
 
 data class AirQualityForecast(val index: Int)
 
 interface AirQualityForecastService {
-    fun getAirQualityForecast(coordinates: GeoCoordinates): Option<List<AirQualityForecast>>
+    fun getAirQualityForecast(coordinates: GeoCoordinates): Either<ApplicationError, List<AirQualityForecast>>
 }

src/main/kotlin/com/wonderful/freshair/infrastructure/api/OWMAirQualityForecastService.kt
@@ -1,16 +1,16 @@
 package com.wonderful.freshair.infrastructure.api
 
-import arrow.core.None
-import arrow.core.Option
-import arrow.core.Some
-import arrow.core.toOption
-import com.wonderful.freshair.domain.AirQualityForecast
-import com.wonderful.freshair.domain.AirQualityForecastService
-import com.wonderful.freshair.domain.GeoCoordinates
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.wonderful.freshair.domain.AirQualityForecast
+import com.wonderful.freshair.domain.AirQualityForecastService
+import com.wonderful.freshair.domain.GeoCoordinates
+import com.wonderful.freshair.domain.error.ApplicationError
 import java.net.URL
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -30,7 +30,7 @@ class OWMAirQualityForecastService(
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     }
 
-    override fun getAirQualityForecast(coordinates: GeoCoordinates): Option<List<AirQualityForecast>> {
+    override fun getAirQualityForecast(coordinates: GeoCoordinates): Either<ApplicationError, List<AirQualityForecast>> {
         val request = HttpRequest.newBuilder()
             .uri(URL(
                 baseUrl,
@@ -42,13 +42,12 @@ class OWMAirQualityForecastService(
         val response: HttpResponse<String> = HttpClient.newHttpClient()
             .send(request, HttpResponse.BodyHandlers.ofString())
 
-        val airQualityForecasts: OMWAirQualityForecasts = objectMapper.readValue(response.body())
-
-        return airQualityForecasts
+        val forecasts = objectMapper.readValue<OMWAirQualityForecasts>(response.body())
             .list
             .map { AirQualityForecast(it.main.aqi) }
-            .ifEmpty { null }
-            .toOption()
+
+        return if (forecasts.isEmpty()) ApplicationError().left() else forecasts.right()
+
     }
 
 }

src/test/kotlin/com/wonderful/freshair/domain/AirQualityForecastServiceTest.kt
@@ -1,15 +1,16 @@
 package com.wonderful.freshair.domain
 
-import arrow.core.None
-import arrow.core.Some
+import arrow.core.left
+import arrow.core.right
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import assertk.assertions.isNull
 import com.wonderful.freshair.infrastructure.api.OWMAirQualityForecastService
+import assertk.assertions.isInstanceOf
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer
+import com.wonderful.freshair.domain.error.ApplicationError
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -60,11 +61,11 @@ class AirQualityForecastServiceTest {
 
         val forecasts = airQualityForecastService.getAirQualityForecast(GeoCoordinates(lat, lon))
 
-        assertThat(forecasts).isEqualTo(Some(expectedAirQualityForecasts))
+        assertThat(forecasts).isEqualTo(expectedAirQualityForecasts.right())
     }
 
     @Test
-    fun `should return none if pollution data is empty`() {
+    fun `should return left if pollution data is empty`() {
         val lat = 41.3888
         val lon = 2.159
         WireMock.stubFor(
@@ -79,6 +80,6 @@ class AirQualityForecastServiceTest {
 
         val airQualityForecasts = airQualityForecastService.getAirQualityForecast(GeoCoordinates(lat, lon))
 
-        assertThat(airQualityForecasts).isEqualTo(None)
+        assertThat(airQualityForecasts).isInstanceOf(ApplicationError().left()::class)
     }
 }