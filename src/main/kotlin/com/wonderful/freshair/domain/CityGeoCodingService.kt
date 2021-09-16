ivan-lorenz/ada-freshair

City geo coding service uses now Either

src/main/kotlin/com/ada/freshair/domain/CityGeoCodingService.kt
@@ -1,11 +1,12 @@
 package com.ada.freshair.domain
 
-import arrow.core.Option
+import arrow.core.Either
+import com.ada.freshair.domain.error.ApplicationError
 import com.ada.freshair.infrastructure.City
 
 data class GeoCoordinates(val lat: Double, val lon: Double)
 data class CityGeoCoded(val name: String, val countryCode: String, val coordinates: GeoCoordinates)
 
 interface CityGeoCodingService {
-    fun getGeoCoordinates(city: City): Option<CityGeoCoded>
+    fun getGeoCoordinates(city: City): Either<ApplicationError, CityGeoCoded>
 }

src/main/kotlin/com/ada/freshair/infrastructure/api/OWMCityGeoCodingService.kt
@@ -1,9 +1,10 @@
 package com.ada.freshair.infrastructure.api
 
-import arrow.core.Option
+import arrow.core.Either
 import com.ada.freshair.domain.CityGeoCoded
 import com.ada.freshair.domain.CityGeoCodingService
 import com.ada.freshair.domain.GeoCoordinates
+import com.ada.freshair.domain.error.ApplicationError
 import com.ada.freshair.infrastructure.City
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -27,7 +28,7 @@ class OWMCityGeoCodingService(
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     }
 
-    override fun getGeoCoordinates(city: City): Option<CityGeoCoded> {
+    override fun getGeoCoordinates(city: City): Either<ApplicationError, CityGeoCoded> {
         val request = HttpRequest.newBuilder()
             .uri(URL(baseUrl, "geo/1.0/direct?q=${city.name},${city.country}&limit=1&appid=${apiKey}").toURI())
             .GET()
@@ -36,7 +37,7 @@ class OWMCityGeoCodingService(
         val response: HttpResponse<String> = HttpClient.newHttpClient()
             .send(request, BodyHandlers.ofString())
 
-        return Option.fromNullable(
+        return Either.fromNullable(
             objectMapper.readValue<List<OMWCity>>(response.body())
             .map {
                 CityGeoCoded(
@@ -44,9 +45,9 @@ class OWMCityGeoCodingService(
                     city.country,
                     GeoCoordinates(it.lat, it.lon)
                 )
-            }
-            .firstOrNull()
-        )
+            }.firstOrNull()
+        ).mapLeft { ApplicationError() }
+
     }
 
 }

src/test/kotlin/com/ada/freshair/domain/CityGeoCodingServiceTest.kt
@@ -1,11 +1,13 @@
 package com.ada.freshair.domain
 
-import arrow.core.None
-import arrow.core.orElse
+import arrow.core.getOrHandle
+import arrow.core.left
 import assertk.assertAll
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
 import assertk.fail
+import com.ada.freshair.domain.error.ApplicationError
 import com.ada.freshair.infrastructure.City
 import com.ada.freshair.infrastructure.api.OWMCityGeoCodingService
 import com.github.tomakehurst.wiremock.WireMockServer
@@ -68,7 +70,7 @@ class CityGeoCodingServiceTest {
                     assertThat(it.countryCode).isEqualTo(cityCountry)
                     assertThat(it.coordinates).isEqualTo(GeoCoordinates(lat, lon))
                 }
-            }.orElse { fail("Should no be None.") }
+            }.getOrHandle { fail("Should no be None.") }
     }
 
     @Test
@@ -83,6 +85,6 @@ class CityGeoCodingServiceTest {
                 )
         )
 
-        assertThat(cityGeoCodingService.getGeoCoordinates(city)).isEqualTo(None)
+        assertThat(cityGeoCodingService.getGeoCoordinates(city)).isInstanceOf(ApplicationError().left()::class)
     }
 }

AdevintaSpain/wonderful-freshair-app

src/main/kotlin/com/wonderful/freshair/domain/CityGeoCodingService.kt
@@ -1,11 +1,12 @@
 package com.wonderful.freshair.domain
 
-import arrow.core.Option
 import com.wonderful.freshair.infrastructure.City
+import arrow.core.Either
+import com.wonderful.freshair.domain.error.ApplicationError
 
 data class GeoCoordinates(val lat: Double, val lon: Double)
 data class CityGeoCoded(val name: String, val countryCode: String, val coordinates: GeoCoordinates)
 
 interface CityGeoCodingService {
-    fun getGeoCoordinates(city: City): Option<CityGeoCoded>
+    fun getGeoCoordinates(city: City): Either<ApplicationError, CityGeoCoded>
 }

src/main/kotlin/com/wonderful/freshair/infrastructure/api/OWMCityGeoCodingService.kt
@@ -1,16 +1,16 @@
 package com.wonderful.freshair.infrastructure.api
 
-import arrow.core.Option
 import arrow.core.firstOrNone
-import arrow.core.toOption
 import com.wonderful.freshair.domain.CityGeoCoded
 import com.wonderful.freshair.domain.CityGeoCodingService
 import com.wonderful.freshair.domain.GeoCoordinates
 import com.wonderful.freshair.infrastructure.City
+import arrow.core.Either
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.wonderful.freshair.domain.error.ApplicationError
 import java.net.URL
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -29,7 +29,7 @@ class OWMCityGeoCodingService(
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     }
 
-    override fun getGeoCoordinates(city: City): Option<CityGeoCoded> {
+    override fun getGeoCoordinates(city: City): Either<ApplicationError, CityGeoCoded> {
         val request = HttpRequest.newBuilder()
             .uri(URL(baseUrl, "geo/1.0/direct?q=${city.name},${city.country}&limit=1&appid=${apiKey}").toURI())
             .GET()
@@ -38,15 +38,16 @@ class OWMCityGeoCodingService(
         val response: HttpResponse<String> = HttpClient.newHttpClient()
             .send(request, BodyHandlers.ofString())
 
-        val omwCity: List<OMWCity> = objectMapper.readValue(response.body())
-
-        return omwCity
-            .map { CityGeoCoded(
-                it.name,
-                it.country,
-                GeoCoordinates( it.lat, it.lon)
-            ) }
-            .firstOrNone()
+        return Either.fromNullable(
+            objectMapper.readValue<List<OMWCity>>(response.body())
+            .map {
+                CityGeoCoded(
+                    city.name,
+                    city.country,
+                    GeoCoordinates(it.lat, it.lon)
+                )
+            }.firstOrNull()
+        ).mapLeft { ApplicationError() }
     }
 
 }

src/test/kotlin/com/wonderful/freshair/domain/CityGeoCodingServiceTest.kt
@@ -1,14 +1,13 @@
 package com.wonderful.freshair.domain
 
-import arrow.core.None
-import arrow.core.orElse
+import arrow.core.getOrHandle
+import arrow.core.left
 import assertk.assertAll
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import assertk.assertions.isNull
-import assertk.assertions.prop
 import com.wonderful.freshair.infrastructure.City
 import com.wonderful.freshair.infrastructure.api.OWMCityGeoCodingService
+import assertk.assertions.isInstanceOf
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
@@ -18,6 +17,7 @@ import com.github.tomakehurst.wiremock.client.WireMock.stubFor
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.options
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer
+import com.wonderful.freshair.domain.error.ApplicationError
 import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
@@ -71,11 +71,11 @@ class CityGeoCodingServiceTest {
                     assertThat(it.countryCode).isEqualTo(cityCountry)
                     assertThat(it.coordinates).isEqualTo(GeoCoordinates(lat, lon))
                 }
-            }.orElse { fail("Should no be None.") }
+            }.getOrHandle { fail("Should no be None.") }
     }
 
     @Test
-    fun `should return none when city does not exist`() {
+    fun `should return left when city does not exist`() {
         val cityName = "Barcelona"
         val cityCountry = "ES"
         val city = City(name = cityName, country = cityCountry)
@@ -87,6 +87,6 @@ class CityGeoCodingServiceTest {
                 )
         )
 
-        assertThat(cityGeoCodingService.getGeoCoordinates(city)).isEqualTo(None)
+        assertThat(cityGeoCodingService.getGeoCoordinates(city)).isInstanceOf(ApplicationError().left()::class)
     }
 }