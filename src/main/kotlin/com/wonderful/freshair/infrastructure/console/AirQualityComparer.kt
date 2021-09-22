24ab5e98ddbfd0965a0385adf5b0baf65e92a2c2
Ivan Lorenz <ivan.lorenz@adevinta.com>
2021-09-22 09:36:01 +0200

    Minor refactor

--- src/main/kotlin/com/wonderful/freshair/infrastructure/console/AirQualityComparer.kt
+++ src/main/kotlin/com/wonderful/freshair/infrastructure/console/AirQualityComparer.kt
@@ -1,31 +1,28 @@
 package com.wonderful.freshair.infrastructure.console
 
-import arrow.core.Either.Left
-import arrow.core.Either.Right
+import arrow.core.computations.either
+import arrow.core.getOrHandle
 import arrow.core.sequenceEither
 import com.wonderful.freshair.domain.AirQualityIndex
 import com.wonderful.freshair.domain.CityAirQualityService
+import com.wonderful.freshair.domain.error.ApplicationError
 import com.wonderful.freshair.infrastructure.City
 
 class AirQualityComparer(
     private val cityAirQualityService: CityAirQualityService
 ) {
 
-    fun compare(cities: List<String>) {
+    fun compare(cities: List<String>): Unit = either.eager<ApplicationError, Unit> {
         val airQualityIndex = cities
             .map { City.fromParameter(it) }
             .map { cityAirQualityService.averageIndex(it) }
             .sequenceEither()
-
-        when (airQualityIndex) {
-            is Left -> println("Cannot compare air quality due to error ${airQualityIndex.value.javaClass.simpleName}.")
-            is Right -> {
-                val cleanestCity = airQualityIndex.value.maxWithOrNull(Comparator.comparing(AirQualityIndex::index))
-                if (cleanestCity != null)
-                    println("${cleanestCity.cityName} has the cleaner air quality index.")
-                else
-                    println("City list is empty.")
-            }
-        }
+            .bind()
+        val cleanestCity = airQualityIndex.maxWithOrNull(Comparator.comparing(AirQualityIndex::index))
+        if (cleanestCity != null)
+            println("${cleanestCity.cityName} has the cleaner air quality index.")
+        else
+            println("City list is empty.")
     }
+    .getOrHandle { error -> println("Cannot compare air quality due to error ${error.javaClass.simpleName}.")}
 }
