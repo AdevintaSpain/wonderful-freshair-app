f88888188b7c566db30c995412e2847eb98ff03b
Ivan Lorenz <ivan.lorenz@adevinta.com>
2021-11-10 18:49:44 +0100

    AirQualityComparer refactored

--- src/main/kotlin/com/wonderful/freshair/infrastructure/console/AirQualityComparer.kt
+++ src/main/kotlin/com/wonderful/freshair/infrastructure/console/AirQualityComparer.kt
@@ -1,29 +1,22 @@
 package com.wonderful.freshair.infrastructure.console
 
 import arrow.core.NonEmptyList
-import arrow.core.computations.either
-import arrow.core.getOrHandle
 import arrow.core.sequenceEither
-import com.wonderful.freshair.domain.AirQualityIndex
 import com.wonderful.freshair.domain.CityAirQualityService
-import com.wonderful.freshair.domain.error.ApplicationError
+import com.wonderful.freshair.domain.minAirQualityIndex
 import com.wonderful.freshair.infrastructure.City
 
 class AirQualityComparer(
     private val cityAirQualityService: CityAirQualityService
 ) {
 
-    fun compare(cities: NonEmptyList<String>): Unit = either.eager<ApplicationError, Unit> {
-        val airQualityIndex = cities
+    fun compare(cities: NonEmptyList<String>): Unit =
+        cities
             .map { City.fromParameter(it) }
             .map { cityAirQualityService.averageIndex(it) }
             .sequenceEither()
-            .bind()
-        val cleanestCity = airQualityIndex.minWithOrNull(Comparator.comparing(AirQualityIndex::index))
-        if (cleanestCity != null)
-            println("${cleanestCity.cityName} has the cleaner air quality index.")
-        else
-            println("City list is empty.")
-    }
-    .getOrHandle { error -> println("Cannot compare air quality due to error ${error.javaClass.simpleName}.")}
+            .fold(
+                { error -> println("Cannot compare air quality due to error ${error.javaClass.simpleName}.") },
+                { indexes -> println("${indexes.minAirQualityIndex().cityName} has the cleaner air quality index.")}
+            )
 }
